0034fd1e7a803701f7aad24e7c62353031c270a4
Lawrence Wilson II <ljwilsonstar2@gmail.com>
2026-03-24 19:16:44 -0500

    adding wireframe details to landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -10,7 +10,19 @@ function LandingPage(){
     </header>
     <nav>
         <ul>
-            <h2>do your magic here</h2>
+            <h2>User Name</h2>
+        </ul>
+        <ul>
+            <h2>Home</h2>
+        </ul>
+         <ul>
+            <h2>Upload Files</h2>
+        </ul>
+        <ul>
+            <h2>Admin Privillegs</h2>
+        </ul>
+        <ul>
+            <h2>Sign Out</h2>
         </ul>
     </nav>
     <div>
