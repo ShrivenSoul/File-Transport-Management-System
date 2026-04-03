7e32b7bc8c3653d91e1a9a91617946beb9161ae6
Koi McManis <koimcmanis@gmail.com>
2026-04-02 21:57:18 -0500

    Added file upload to frontend, renamed some URLs

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LandingPage.css";
 
 function LandingPage(){
+    const [setSelectedFile] = useState(null);
+    const onFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+        console.log(event.target.files[0]);
+    }
+    const onFileUpload = async (event) => {
+            console.log("File sent!");
+            const formData = new FormData(document.querySelector("#fileInput"));
+            console.log(formData);
+            // REPLACE LINK HERE WITH WHERE YOUR SERVER IS RUNNING
+            const response = await fetch("http://localhost:3000/upload", {
+                method: "POST",
+                body: formData,
+            }).then((response) => response.json()).then((data) => {console.log(data);});
+            console.log(response);
+            event.preventDefault();
+        }
+
     return (<>
     <header>
         <div>
@@ -17,6 +35,11 @@ function LandingPage(){
         </ul>
     </nav>
     <div>
+        <h3>Upload file here: </h3>
+        <form encType="multipart/form-data" method="post" name="fileInput" id="fileInput">
+            <input type="file" name="file" onChange={onFileChange} />
+        </form>
+        <button onClick={onFileUpload}>Send to server!</button>
         <p>| nav ends here</p>
     </div>
     </>);
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/CDS-capstone" element={<App/>}/>
+        <Route path="/" element={<App/>}/>
         <Route path="/home" element={<LandingPage/>}/>
       </Routes>
     </Router>
